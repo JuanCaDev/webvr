890b06d2404216e8d7662c8be8f6bc95891ee074
JuanCaDev <juancafelizzola@gmail.com>
2019-03-13 11:39:04 -0500

    Agregando dependencias y componentes

--- proyectogrado/src/app/app.module.ts
+++ proyectogrado/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent,
+    RegisterPageComponent,
+    PrivatePageComponent,
+    NotFoundPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
