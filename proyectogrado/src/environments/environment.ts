9beffa2809bcc44f34a6a3b95d98d52a35ec1ff8
JuanCaDev <juancafelizzola@gmail.com>
2019-03-15 22:27:47 -0500

    FIrebase Config

--- proyectogrado/src/environments/environment.ts
+++ proyectogrado/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Initialize Firebase
+  firebaseConfig: {
+    apiKey: "AIzaSyBGjIOndpmIsN_dE3BrhlFlFvVut5ACTgU",
+    authDomain: "proyectogrado-36503.firebaseapp.com",
+    databaseURL: "https://proyectogrado-36503.firebaseio.com",
+    projectId: "proyectogrado-36503",
+    storageBucket: "proyectogrado-36503.appspot.com",
+    messagingSenderId: "159470960798"
+  }
+  // firebase.initializeApp(config);
 };
 
 /*
